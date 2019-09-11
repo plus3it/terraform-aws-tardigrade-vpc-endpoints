8d0bec097d5b31b62f01a4fb698295d686f72dc6
confusdcodr <confusdcodr@users.noreply.github.com>
2019-09-11 08:59:53 -0400

    Upgrade to terraform 0.12.x

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Terraform module to create VPC Endpoints
 
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
-| create\_vpc\_endpoints | toggle to create vpc endpoints | string | `"false"` | no |
-| subnet\_ids | target subnet ids | list | `<list>` | no |
-| tags | A map of tags to add to the VPC endpoint SG | map | `<map>` | no |
-| vpc\_endpoint\_interfaces | List of aws api endpoints that are used to create VPC Interface endpoints. See https://docs.aws.amazon.com/general/latest/gr/rande.html for full list. | list | `<list>` | no |
+| create\_vpc\_endpoints | toggle to create vpc endpoints | string | `"true"` | no |
+| subnet\_ids | target subnet ids | list(string) | `<list>` | no |
+| tags | A map of tags to add to the VPC endpoint SG | map(string) | `<map>` | no |
+| vpc\_endpoint\_interfaces | List of aws api endpoints that are used to create VPC Interface endpoints. See https://docs.aws.amazon.com/general/latest/gr/rande.html for full list. | list(string) | `<list>` | no |
 
--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 data "aws_subnet" "selected" {
-  count = "${var.create_vpc_endpoints ? 1 : 0}"
+  count = var.create_vpc_endpoints ? 1 : 0
 
-  id = "${var.subnet_ids[0]}"
+  id = var.subnet_ids[0]
 }
 
 data "aws_vpc" "selected" {
-  count = "${var.create_vpc_endpoints ? 1 : 0}"
+  count = var.create_vpc_endpoints ? 1 : 0
 
-  id = "${local.vpc_id}"
+  id = local.vpc_id
 }
 
 locals {
-  vpc_id   = "${join("", data.aws_subnet.selected.*.vpc_id)}"
-  vpc_cidr = "${join("", data.aws_vpc.selected.*.cidr_block)}"
+  vpc_id   = join("", data.aws_subnet.selected.*.vpc_id)
+  vpc_cidr = join("", data.aws_vpc.selected.*.cidr_block)
 }
 
 resource "aws_security_group" "this" {
-  count = "${var.create_vpc_endpoints ? 1 : 0}"
+  count = var.create_vpc_endpoints ? 1 : 0
 
   description = "VPC Interface Endpoints - Allow inbound from ${local.vpc_id} and allow all outbound"
-  vpc_id      = "${local.vpc_id}"
+  vpc_id      = local.vpc_id
 
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["${local.vpc_cidr}"]
+    cidr_blocks = [local.vpc_cidr]
   }
 
   egress {
@@ -35,24 +35,23 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "aws_vpc_endpoint" "interface_services" {
-  count = "${var.create_vpc_endpoints ? length(var.vpc_endpoint_interfaces) : 0}"
+  count = var.create_vpc_endpoints ? length(var.vpc_endpoint_interfaces) : 0
 
-  vpc_id            = "${local.vpc_id}"
-  service_name      = "${var.vpc_endpoint_interfaces[count.index]}"
+  vpc_id            = local.vpc_id
+  service_name      = var.vpc_endpoint_interfaces[count.index]
   vpc_endpoint_type = "Interface"
   auto_accept       = true
 
-  subnet_ids = [
-    "${var.subnet_ids}",
-  ]
+  subnet_ids = var.subnet_ids
 
   security_group_ids = [
-    "${aws_security_group.this.id}",
+    aws_security_group.this[0].id,
   ]
 
   private_dns_enabled = true # https://docs.aws.amazon.com/vpc/latest/userguide/vpce-interface.html#vpce-private-dns
 }
+
