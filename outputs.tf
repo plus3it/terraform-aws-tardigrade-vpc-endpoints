983cf5c1581c204d2a2c50d5e5c9fa76dffc7ba1
Evan Stachowiak <evanstachowiak@users.noreply.github.com>
2019-10-10 11:24:37 +0200

    Add support for sgs per endpoint.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
+output "vpc_endpoint_sgs" {
+  value = aws_security_group.this
+}
 
 output "vpc_endpoint_interface_services" {
   value = aws_vpc_endpoint.interface_services
--- variables.tf
+++ variables.tf
@@ -1,7 +1,25 @@
+variable "create_sg_per_endpoint" {
+  description = "toggle to create a sg for each vpc endpoint. Defaults to using just one for all endpoints."
+  type        = bool
+  default     = false
+}
+
 variable "create_vpc_endpoints" {
   description = "toggle to create vpc endpoints"
   type        = bool
   default     = true
+}
+
+variable "sg_egress_rules" {
+  description = "Egress rules for the vpc endpoint sg(s). Set to empty list to disable default rules."
+  type        = list
+  default     = null
+}
+
+variable "sg_ingress_rules" {
+  description = "Ingress rules for the vpc endpoint sg(s). Set to empty list to disable default rules."
+  type        = list
+  default     = null
 }
 
 variable "subnet_ids" {
