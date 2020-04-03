0073564398a06262309603d4b9045b509c018acd
Matt Klich <matt.klich@ibotta.com>
2020-04-02 21:16:52 -0600

    Add more tests!

---
+++ tests/fully_qualified_name_endpoint/README.md
@@ -0,0 +1,19 @@
+# Fully Qualified Name Endpoint Test
+
+
+<!-- BEGIN TFDOCS -->
+## Providers
+
+| Name | Version |
+|------|---------|
+| random | n/a |
+
+## Inputs
+
+No input.
+
+## Outputs
+
+No output.
+
+<!-- END TFDOCS -->
---
+++ tests/fully_qualified_name_endpoint/main.tf
@@ -0,0 +1,35 @@
+provider aws {
+  region = "us-east-1"
+}
+
+resource "random_string" "this" {
+  length  = 6
+  upper   = false
+  special = false
+  number  = false
+}
+
+module "vpc" {
+  source = "github.com/terraform-aws-modules/terraform-aws-vpc?ref=v2.15.0"
+  providers = {
+    aws = aws
+  }
+
+  name                 = "tardigrade-vpc-endpoints-${random_string.this.result}"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["us-east-1a", "us-east-1b"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+module "fully_qualified_name_endpoint" {
+  source = "../../"
+  providers = {
+    aws = aws
+  }
+
+  create_vpc_endpoints  = true
+  vpc_endpoint_services = ["aws.sagemaker.us-east-1.notebook"]
+  subnet_ids            = module.vpc.private_subnets
+}
